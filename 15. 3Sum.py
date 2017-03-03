482aba013a7d8ea352489eed937cf1c568bf0c89
wukun03 <farseerwk@qq.com>
2017-03-03 11:20:51 +0800

    2017-3-3: 15AC

--- 15. 3Sum.py
+++ 15. 3Sum.py
@@ -7,39 +7,32 @@ class Solution(object):
         if len(nums) == 0:
             return []
         nums.sort()
-        #print nums
         re = []
-        d = dict()
+        last = nums[0] + 1
         for i in range(0,len(nums)):
-            if d.has_key(nums[i]):
-                d[nums[i]] = d[nums[i]] + 1
-            else:
-                d[nums[i]] = 1
-
-        #print d
-        last_i = nums[0] + 1
-        for i in range(0,len(nums) - 1):
-            if nums[i] == last_i:
+            if nums[i] == last:
                 continue
-            last_i = nums[i]
-            last_j = nums[i + 1] + 1
-            for j in range(i + 1,len(nums)):
-                if nums[j] == last_j:
-                    continue
-                last_j = nums[j]
-                k = 0 - nums[i] - nums[j]
-                d[nums[i]] = d[nums[i]] - 1
-                d[nums[j]] = d[nums[j]] - 1
-                tmp = []
-                if d.has_key(k) and d[k] > 0:
-                    #print i,j,k
+            last = nums[i]
+            j = i + 1
+            k = len(nums) - 1
+            while j < k:
+                n = nums[i]
+                sum = nums[j] + nums[k]
+                if sum > -n:
+                    k = k - 1
+                elif sum < -n:
+                    j = j + 1
+                else:
+                    tmp = []
                     tmp.append(nums[i])
                     tmp.append(nums[j])
-                    tmp.append(k)
+                    tmp.append(nums[k])
                     re.append(tmp)
-                d[nums[i]] = d[nums[i]] + 1
-                d[nums[j]] = d[nums[j]] + 1
-        #print re
+                    t= nums[j]
+                    j = j + 1
+                    while j < len(nums) and nums[j] == t:
+                        j = j + 1
+
         return re
 
 if __name__ == '__main__':
